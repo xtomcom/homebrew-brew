89241b52eeabe108a5e34fd12c553bbdd21cb8db
Showfom <6773591+Showfom@users.noreply.github.com>
2026-03-01 06:54:01 +0000

    chore: update formula to latest release

--- Formula/rdap.rb
+++ Formula/rdap.rb
@@ -1,13 +1,13 @@
 class Rdap < Formula
   desc "A command-line RDAP client for querying domain, IP, and ASN information"
   homepage "https://github.com/xtomcom/rdap"
-  version "1.0.3"
+  version "1.0.4"
   if Hardware::CPU.arm?
     url "https://github.com/xtomcom/rdap/releases/download/v#{version}/rdap-#{version}-macos-aarch64"
-    sha256 "ccb554a08413187dfdc4f478e50682e2e6fe22c2f6ee0f646994039c47804442"
+    sha256 "26c9c4246c9deb64d5833b68e5d0b7d1ee0585f5250b869491065ebeeb86897a"
   else
     url "https://github.com/xtomcom/rdap/releases/download/v#{version}/rdap-#{version}-macos-x86_64"
-    sha256 "edaf2d300d4ebb9800de6f658ac328fa7839f96b9b7dc147cbbf0da30a07484e"
+    sha256 "f131a55c90a56b5367ded781c9feb01f345bcd8644d82cee05acbd37abb77b9d"
   end
   license "MIT"
 
